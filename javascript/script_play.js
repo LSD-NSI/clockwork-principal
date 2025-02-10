c45ddb9c02e56493652984b122f6eb274bc43d93
LSD <194146983+LSD-NSI@users.noreply.github.com>
2025-02-10 17:14:43 +0100

    enelver f6

--- javascript/script_play.js
+++ javascript/script_play.js
@@ -121,7 +121,7 @@ document.addEventListener("contextmenu", function (event) {
 });
 
 document.addEventListener("keydown", function (event) {
-    if (event.key === "F12", "F6" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+    if (event.key === "F12",|| (event.ctrlKey && event.shiftKey && event.key === "I")) {
         event.preventDefault();
         showPopup();
     }
